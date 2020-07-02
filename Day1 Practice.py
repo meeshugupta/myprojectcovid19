980cd96b9f3d622393879b850bf6f9909d9bf3d2
meeshugupta <67697020+meeshugupta@users.noreply.github.com>
2020-07-02 16:45:10 +0530

    Add files via upload

---
+++ Day1 Practice.py
@@ -0,0 +1,453 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Hello
+
+
+# In[2]:
+
+
+"Hello"
+
+
+# In[1]:
+
+
+"4+5"
+
+
+# In[2]:
+
+
+4+5
+
+
+# In[6]:
+
+
+A="BACTERIA"
+B="VIRUS"
+print("A. CORONA is {}".format(B))
+
+
+# #type function used to find out data type
+
+# In[22]:
+
+
+a="udyogini"
+b="women"
+c='organization'
+print("what is {}?" .format(a))
+
+
+# In[23]:
+
+
+print("A is {} which is {} based {}".format (a,b,c))
+
+
+# In[24]:
+
+
+print("Q is {} which is {} based {}".format (a,b,c))
+
+
+# In[25]:
+
+
+print(" is {} which is {} based {}" .format (a,b,c))
+
+
+# In[26]:
+
+
+a=2
+b=2
+c=a/b
+print(c)
+
+
+# In[27]:
+
+
+a=1000000
+b=234567
+c=234670976
+print((a-b)/c)
+
+
+# In[28]:
+
+
+a=1000000
+b=234567
+c=234670976
+print((a*b)/c)
+
+
+# In[29]:
+
+
+type(false)
+
+
+# In[31]:
+
+
+total_migrants=5690
+total_migrants_today=4500
+total_migrants_yesterday=456
+District="Sehdol"
+print(type(total_migrants))
+print(District)
+
+
+# In[32]:
+
+
+increment_migrants=total_migrants_today-total_migrants_yesterday
+print(increment_migrants)
+
+
+# In[34]:
+
+
+percentage_increment=((total_migrants_today-total_migrants_yesterday)/total_migrants_yesterday)*100
+print(percentage_increment)
+
+
+# In[35]:
+
+
+a="covid"
+b="19"
+c=a+b
+print(c)
+
+
+# In[37]:
+
+
+a="covid"
+b="19"
+c=a+b
+print(c)
+
+
+# In[38]:
+
+
+A="INDIA"
+
+
+# In[39]:
+
+
+A[0:2]
+
+
+# In[40]:
+
+
+A[:4]
+
+
+# In[41]:
+
+
+A[1:6]
+
+
+# In[42]:
+
+
+A[2:]
+
+
+# In[43]:
+
+
+A[2:4]
+
+
+# In[44]:
+
+
+A[1:5]
+
+
+# In[45]:
+
+
+A[1:6]
+
+
+# In[46]:
+
+
+A="CORONA"
+
+
+# In[47]:
+
+
+A[0]
+
+
+# In[48]:
+
+
+A[1]
+
+
+# In[49]:
+
+
+A[2]
+
+
+# In[50]:
+
+
+A[3]
+
+
+# In[51]:
+
+
+A[4]
+
+
+# In[52]:
+
+
+A[5]
+
+
+# In[53]:
+
+
+A[6]
+
+
+# In[54]:
+
+
+A[1:5]
+
+
+# In[55]:
+
+
+A[1:6]
+
+
+# In[56]:
+
+
+A[3:5]
+
+
+# In[57]:
+
+
+A[2:3]
+
+
+# In[58]:
+
+
+A[5:]
+
+
+# In[59]:
+
+
+A[4:]
+
+
+# In[60]:
+
+
+A[::2]
+
+
+# In[61]:
+
+
+A[::1]
+
+
+# In[62]:
+
+
+A[2:]
+
+
+# In[63]:
+
+
+A[1:]
+
+
+# In[64]:
+
+
+A[2:]
+
+
+# In[65]:
+
+
+A[4:]
+
+
+# In[66]:
+
+
+A[:4]
+
+
+# In[67]:
+
+
+A[3:]
+
+
+# In[68]:
+
+
+A[2:]
+
+
+# In[69]:
+
+
+A[:2]
+
+
+# In[70]:
+
+
+A[:1]
+
+
+# In[71]:
+
+
+A[2:]
+
+
+# In[72]:
+
+
+A[:3]
+
+
+# In[73]:
+
+
+A[2:4]
+
+
+# In[74]:
+
+
+A[2:]
+
+
+# In[75]:
+
+
+A[:2]
+
+
+# In[76]:
+
+
+Strings
+
+
+# In[77]:
+
+
+"india"
+
+
+# In[78]:
+
+
+"This is also a string"
+
+
+# In[79]:
+
+
+"we can also use double quote"
+
+
+# In[80]:
+
+
+"1111"
+
+
+# In[83]:
+
+
+a="1111"
+b="23234"
+print(a,b)
+print("1111","23234")
+
+
+# In[84]:
+
+
+len{"total   13"}
+
+
+# In[86]:
+
+
+len('jdsf     fdslkj')
+
+
+# In[90]:
+
+
+print(district)
+
+
+# In[108]:
+
+
+total_till_date=1234
+infected_on_10_6_june=12
+infected_on_11_6_june=2
+infected_on_12_6_june=13
+District="Gwalior"
+print(type(total_till_date))
+
+
+# In[111]:
+
+
+percentage_change=((infected_on_12_6_june-infected_on_10_6_june)/infected_on_12_6_june)*100
+
+
+# In[114]:
+
+
+print(percentage_change)
+
+
+# In[ ]:
+
+
+
+
