9ce5ce55e776f6eea3215bda835ed2feae0e8d1f
meeshugupta <67697020+meeshugupta@users.noreply.github.com>
2020-07-03 13:05:56 +0530

    Add files via upload

---
+++ Day2 practice (1).py
@@ -0,0 +1,564 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=7
+b=7
+
+
+# In[2]:
+
+
+a==b
+
+
+# In[3]:
+
+
+b=8
+c=9
+
+
+# In[4]:
+
+
+b==c
+
+
+# In[2]:
+
+
+my_dict={"Jharkhand":"Lac","Madhya Pradesh":"SHG","Chhattisgarh":"Livestock","Rajasthan":"Agriculture","Uttarakhand":"MAP"}
+
+
+# In[21]:
+
+
+my_dict.keys()
+
+
+# In[24]:
+
+
+my_dict.values()
+
+
+# In[22]:
+
+
+lst = [1,2,3,4]
+
+
+# In[25]:
+
+
+my_dict.items()
+
+
+# In[35]:
+
+
+my_dict={"Jharkhand":"Lac","Madhya Pradesh":["SHG","Education","Health"],"Chhattisgarh":["Livestock", "Lac", "Agri", "MAP"],"Rajasthan":"Agriculture","Uttarakhand":"MAP"}
+
+
+# In[34]:
+
+
+my_dict["Madhya Pradesh"]
+
+
+# In[37]:
+
+
+my_dict["Chhattisgarh"]
+
+
+# In[38]:
+
+
+my_dict["Chhattisgarh"][0]
+
+
+# In[39]:
+
+
+my_dict["Madhya Pradesh"][2]
+
+
+# In[40]:
+
+
+my_dict["Chhattisgarh"][1]
+
+
+# In[43]:
+
+
+my_dict["Jharkhand"]
+
+
+# In[44]:
+
+
+my_dict["Jharkhand"][0].upper()
+
+
+# In[45]:
+
+
+my_dict["Jharkhand"][0].lower()
+
+
+# In[47]:
+
+
+my_dict["Jharkhand"]
+
+
+# In[48]:
+
+
+print("Madhya Pradesh","Jharkhand")
+
+
+# In[49]:
+
+
+my_dict.items():
+    print("Madhya Pradesh","Jharkhand")
+
+
+# In[50]:
+
+
+my_dict.values()
+
+
+# In[51]:
+
+
+my_dict.keys()
+
+
+# In[52]:
+
+
+my_dict.items()
+
+
+# In[53]:
+
+
+my_dict["Jharkhand"][2]
+
+
+# In[56]:
+
+
+my_dict["Madhya Pradesh"][2]
+
+
+# In[57]:
+
+
+my_dict["Jharkhand"][0]
+
+
+# In[58]:
+
+
+my_dict["Jharkhand"][1]
+
+
+# In[59]:
+
+
+my_key={"key1":234,"key2":123,"key3":456}
+
+
+# In[63]:
+
+
+my_key["key1"]=["key1"]-123
+
+
+# In[65]:
+
+
+d={}
+
+
+# In[67]:
+
+
+d["Udyogini"]="organization"
+
+
+# In[68]:
+
+
+d
+
+
+# In[71]:
+
+
+my_dict={"Jharkhand":"Lac","Madhya Pradesh":["SHG","Education","Health:["women_health","children_health"]
+
+
+# In[72]:
+
+
+my_dict={"HR":"Raina","Finance":"vikash","MEL":"Meeshu"}
+
+
+# In[73]:
+
+
+my_dict.values()
+
+
+# In[74]:
+
+
+my_dict.keys()
+
+
+# In[76]:
+
+
+my_dict.items()
+
+
+# In[77]:
+
+
+t=(1,2,3,4,5)
+
+
+# In[78]:
+
+
+len(t)
+
+
+# In[79]:
+
+
+t=("jharkhand","uttarakhand","rajastan")
+
+
+# In[80]:
+
+
+len(t)
+
+
+# In[87]:
+
+
+t=("jharkhand","uttarakhand","rajastan",1,2,3)
+
+
+# In[85]:
+
+
+len(t)
+
+
+# In[83]:
+
+
+t(0)
+
+
+# In[84]:
+
+
+t(1)
+
+
+# In[86]:
+
+
+t.index(0)
+
+
+# In[88]:
+
+
+lis=(1,2,3,4)
+
+
+# In[93]:
+
+
+t(0)="change"
+
+
+# In[94]:
+
+
+a=(1,2,3,4)
+
+
+# In[95]:
+
+
+set(a)
+
+
+# In[103]:
+
+
+print(set(a))
+
+
+# In[106]:
+
+
+set(a).add(5)
+
+
+# In[107]:
+
+
+print(set(a))
+
+
+# In[110]:
+
+
+a=2
+b=4
+
+
+# In[111]:
+
+
+a==b
+
+
+# In[112]:
+
+
+a<b
+
+
+# In[114]:
+
+
+a=16
+b=4
+
+
+# In[115]:
+
+
+a==b*4
+
+
+# In[116]:
+
+
+a=25
+b=1
+
+
+# In[117]:
+
+
+a==b*25
+
+
+# In[118]:
+
+
+a=1
+b=30
+
+
+# In[119]:
+
+
+a==b/30
+
+
+# In[120]:
+
+
+a==b
+
+
+# In[121]:
+
+
+a=2*3*6
+b=36
+
+
+# In[122]:
+
+
+a==b
+
+
+# In[125]:
+
+
+a="india"
+
+
+# In[126]:
+
+
+loc='cases'
+
+
+# In[136]:
+
+
+state="state"
+if state=="state":
+    
+    print("jharkhand","uttarakhand", "rajasthan","chhattisgarh"):
+        
+elif loc =="place"
+    print("you are from which state"):
+        
+else:
+    print("your organization")
+
+
+# In[ ]:
+
+
+
+
+
+# In[135]:
+
+
+organization="udyogini"
+if organization=="udyogini":
+    
+print("welcome udyogini")
+else:
+print("hello partners of udyogini")
+
+
+# In[9]:
+
+
+my_dict={"Maharashtra":79091,"Tamil Nadu":39859,"Andhra Pradesh":8071,"Arunachal Pradesh":128,"Assam":2719,"bihar":2233}
+
+
+# In[8]:
+
+
+t=(93154,52926,6988,66,5851,7946)
+
+
+# In[11]:
+
+
+my_dict.values()
+
+
+# In[12]:
+
+
+my_dict.items()
+
+
+# In[13]:
+
+
+my_dict.keys()
+
+
+# In[1]:
+
+
+list=["Maharashtra"]
+
+
+# In[2]:
+
+
+print("Maharashtra")
+
+
+# In[14]:
+
+
+len(t)
+
+
+# In[15]:
+
+
+t(0)
+
+
+# In[17]:
+
+
+t.count(1)
+
+
+# In[1]:
+
+
+list=["Maharashtra","Tamil Nadu","Assam", "Arunachal Pradesh", "Rajasthan","Bihar"]
+
+
+# In[2]:
+
+
+print(list)
+
+
+# In[3]:
+
+
+list.sort()
+
+
+# In[4]:
+
+
+print("sorted list:",list)
+
+
+# In[8]:
+
+
+infected=[79091, 2791,2233,3317,39859]
+
+
+# In[9]:
+
+
+infected.sort()
+
+
+# In[10]:
+
+
+print("sorted infected:",infected)
+
+
+# In[11]:
+
+
+print(infected)
+
+
+# In[ ]:
+
+
+
+
